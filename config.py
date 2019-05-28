e0dbc7b5dc21da09d803c46a8a8796da3e373e81
Mark Foster <mark.foster@abracontrols.com>
2019-05-28 13:22:57 -0600

    update config.py

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
 import json
-
+msg = "Error opening config.json, make sure it exists"
 try:
    config = json.loads(open("config.json", "r").read())
+   # test for required settings, for example:
+   # if (config.get('PORT') == None):
+   #    msg ="PORT missing from config"
+   #    quit()
+   # or call get() and catch the KeyError below
+except KeyError:
+   print("Missing required KEY in config.json")
+   quit()
 except:
-   print("Error opening config.json, make sure it exists")
+   print(msg)
    quit()
